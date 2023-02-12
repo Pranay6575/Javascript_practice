4e33102cdc9a5ef5e5d8617550a0d30a01001d8a
Pranay <pranaypatil6575@gmail.com>
2023-02-12 18:37:56 +0530

    Array Methods and Higher Programme Added

--- Array/Arrays_Methods.js
+++ Array/Arrays_Methods.js
@@ -1,28 +1,166 @@
-// let num = [1,2,3,4,5,6,7,8,9]
-// let number = [11,12,13,15,18,19]
-// let num_even_more = [211,212,213,215,218,219]
-// // delete num[2]
-// // console.log(num.length)
+//                   An array is a special variable, which can hold more than one value...!
 
-// let newArray = num.concat(number,num_even_more)
-// console.log(newArray)
-// console.log(num,num_even_more)
+//for example - 
+//let cars = ["Volvo","Suzuki","Honda","Audi"]
 
-// let compare = (a , b)=>{
-//   return a - b
+// You access an array element by referring to the index number:
+// const cars = ["Volvo" , "BMW", "MG", "Ford"]
+// let car = cars[1];
+// console.log(car)
+
+// This statement changes the value of the first element in cars
+// const cars = ["Volvo" , "BMW", "MG", "Ford"]
+// cars[2] ="Mercedes"
+// console.log(cars)
+
+// Arrays are Objects
+// Arrays are a special type of objects. The typeof operator in JavaScript returns "object" for arrays.
+// const employee = ["worker", "operator","Auditor","Helper"]
+// console.log(typeof(employee))
+
+// Objects use names to access its "members". In this example, person.firstName returns John
+// const person = {firstName:"David",lastName:"Philips",age:47}
+// console.log(person)
+
+// myArray[0] = Date.now
+// myArray[1] = myFunction;
+// myArray[2] = myCars;
+
+// The length Proerty
+// The length property of an array returns the length of an array (the number of array elements).
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"]
+// let fruit = fruits.length
+// console.log(fruit);
+
+//How to accessing the last element
+// const banks = ["Hdfc","Sbi","RBl","Std chant","Axis"]
+// let bank = banks[banks.length-1];
+// console.log(bank)
+
+// Looping Array Elements
+// const countries = ["Norway","Canada","Singapore","Malasia"]
+// countries.forEach(function(countries){
+// console.log(countries)
+// })
+
+//
+// const banks = ["Hdfc","Sbi","RBl","Std chant","Axis"]
+// for(let i =1; i<banks.length; i++){
+//     console.log(banks[i])
 // }
-// let num = [48,6,77,24,34,33,11,47]
-//  num.sort(compare)
-// num.reverse()
-// console.log(num)
 
-let number = [11,12,13,15,18,19,20,21,22,23,24,25]
-// number.splice(2,2,121, 242)
-// console.log(number)
+// The easiest way to add a new element to an array is using the push() method:
+// const countries = ["Norway","Canada","Singapore","Malasia"]
+// countries.push("United Kingdom") // Adds new element (UK) in to countries
+// console.log(countries)
 
-let newArr = number.slice(5,8)
-    
-console.log(newArr)
+// New element can also be added to an array using the length property:
+// const vegies =["Beans","Brocoli","Brinjal"]
+// vegies[vegies.length] = "Tomato"
+// console.log(vegies)
+
+// const devices = ["Earphone","Mobile","Printer"]
+// devices.push("Laptop")
+// console.log(devices)
+
+// const games = ["Cricket","FootBall","Tennis","Swimming"];
+// games[6]= "Hockey"
+// console.log(games)
+
+// WHen to use Objects and Numbers 
+// const people = {Job:"Auditor",Dept:"E-com",Years:"3 yr"}
+// console.log(people.Job)
+
+// JavaScript, arrays use numbered indexes.
+// let online = [];
+// online ["rating"] = "Five Stars";
+// online ["Feedback"] = "Excellent";
+// online ["Filter"] = "Low to High";
+// online ["Payment"] = "Debit/Credit";
+
+// console.log(online.rating)
+// Arrays to String Conversion using toString()
+
+// let techCompny = ["Google", "HP", "Dell","Samsung"];
+// techCompny= techCompny.toString();
+// console.log(techCompny)
+
+// Join Method 
+// let shopSites = ["Amazon" , "Flipkart", "Myntra","BlinkIt","JioMart"]
+// let newSites = shopSites.join("-")
+// console.log(newSites)
+
+// Pop Method 
+// let fontStyle = ["Devnagri" ,"Italian","Bold","Calibri","Axial"]
+//  fontStyle =  fontStyle.pop();
+//  console.log(fontStyle)
+
+//Push Method 
+// let searchEngine = ["Google","Safari","Yahoo","Bing"]
+// let length = searchEngine.push("Onion");
+// console.log(searchEngine)
+
+// let cities =["Newyork","Mumbai","LasVegas","Torranto"]
+// let newCities= cities.push("Berlin")
+// console.log(cities)
+
+// Shift() is bit similar of pop() that it removes 1st item instead of last
+// let fruit = ["Dragonfruit","Watermelon","Avacado","Papaya"]
+// console.log(fruit.shift());
+
+//The unshift() method adds a new element to an array (at the beginning), and "unshifts" older elements:
+
+// let animal = ["Cat","Dog","Tiger","Monkey"]
+// animal.unshift("Horse");
+// console.log(animal)
+
+// let firstName = ["Michael","James","David","Albert"]
+// let surName = ["Jakson","Sponser","Mills","Roziki"]
+// let merge = firstName.concat(surName)
+// console.log(merge)
+
+// Slicing method is used to Join or Add new elements and also if you want delete items
+// let autoMobiles = ["Volkwagen","Skoda","Suzuki","Peigon","Renualt","Kia"]
+// autoMobiles.splice(2,2,"Mahindra","Tata")
+// console.log(autoMobiles)
+
+// let bikeAutos = ["TVS","Hero","Yamaha","Jawa"]
+// bikeAutos.splice(3,0,"Ducati","BMW");
+// console.log(bikeAutos)
+
+// The slice() method slices out a piece of an array into a new array.
+
+// let upiApp = ["GPay","PhonePe","PayTm","Airtel","AmazonPay"]
+// let newApp = upiApp.slice(2);
+// console.log(newApp)
+
+// Sorting method in an Array 
+// let greeting =["Good Morning","Good Evening","Good Day","Good Night"]
+// greeting.sort();
+// console.log(greeting);
+
+// let digits =[75,45,25,72,55,83]
+// digits.sort()
+// console.log(digits)
+
+// let Number = [415,94,100,168,264,176,211]
+// Number.sort(function(a,b){return b-a})
+// console.log(Number)
+
+// Sorting an Object Arrays
+
+// let extantFish = [
+//     {name: "Whale Shark", length: "61.7" }
+//     { name: "Basking Shark", length: "46" }
+//     { name: "Tiger Shark", length: "25" }
+//     { name: "White Shark", length: "23" }
+// ];
+// extantFish.sort(function (a, b) { return a.length - b.length })
+
+
+
+
 
 
 
--- Array/Higher_Arrays.js
+++ Array/Higher_Arrays.js
@@ -1,41 +1,228 @@
-// Map () function
-let num = [57,24,47]
-//console.log(num)
+//The map() method creates a new array populated with the results of calling
 
-// let str = num.map((elemnt)=>{
-//   console.log(elemnt) 
-//   return elemnt*2
+// let arr1 = [2,4,6,8,10,12]
+// let newArr = arr1.map(x => x*2)
+// console.log(newArr);
+
+// Multiply all the values in an array with 10:
+
+// let values =[1,5,10,15,20,25];
+// let newValues = values.map(x => x *10);
+// console.log(newValues);
+
+//Prb. 1 
+
+// const players = [
+//     {firstname : "David", lastname: "Malan"},
+//     {firstname: "Joe", lastname: "Root"},
+//     {firstname: "Michael", lastname: "Jordan"}
+
+// ];
+
+// players.map(getfullName);
+
+// function getfullName(element){
+//     return  [element.firstname, element.lastname].join(" ");
+// }
+
+// const persons = [
+//     {firstname : "Malcom", lastname: "Reynolds"},
+//     {firstname : "Kaylee", lastname: "Frye"},
+//     {firstname : "Jayne", lastname: "Cobb"}
+//   ];
+
+//   persons.map(getFullName);
+
+//   function getFullName(item) {
+//     return [item.firstname,item.lastname].join(" ");
+//   }
+
+// Prb .2 
+// let numbers = [20,40,60,80]
+// let newMap = numbers.map((value, index, array)=>{
+//   console.log(value, index, array)
+//   return value *2;
 // })
-// console.log(str)
+// console.log(newMap);
 
-// Map () makes new array 
+// Prb. 3
 
-// let cld = num.map((elmnt, index, array)=>{
-//   console.log(elmnt , index, array)
-//   return elmnt + 10
+// let empName = ["Aryan", "Kumar","Devajit"];
+// let newName = empName.map((first)=>{
+// console.log(first + " Is the best employees");
+// return first     // Without return it cannot shows on console.
 // })
-//  console.log(cld)
+// console.log((newName.join(" & ")));
 
-// Filter method
-let arr1 = [34,45,13,52,12,27,17]
-// let acr = arr1.filter((value)=>{
-//   return value > 20
-// })
-// console.log(acr)
+// Prb 4) Make an array of numbers that are doubles of the first array
 
-// Reduce function
+// function doubleNumbers(arr){
+//   return arr.map(arr => arr*2);
+// }
+// console.log(doubleNumbers([5,10,200]))
 
-let arr3 = [3,5,6,2,9,3]
-// let str3 = arr3.reduce((h1, h2)=>{
-// return h1*h2
-// })
-// console.log(str3)
+// // Prb . 5) Take an array of numbers and make them strings
+// function StringitUp(str){
+//     return str.map(str => str+" ")
+// }
+// console.log(StringitUp([50,100,150]))
 
-const reduce_func = (h1 , h2)=>{ // U can make function inside function
-  return h1+h2
-}
-let func = arr3.reduce(reduce_func)
-console.log(func)
+// //Prb . 6) Make an array of strings of the names
+// function namesOnly(arr){
+//   // your code here
+//   return (arr.map(arr => arr.name, arr.age))
+// }
+
+// console.log(namesOnly([
+//     {
+//         name: "Angelina Jolie",
+//         age: 80
+//     },
+//     {
+//         name: "Eric Jones",
+//         age: 2
+//     },
+//     {
+//         name: "Paris Hilton",
+//         age: 5
+//     },
+//     {
+//         name: "Kayne West",
+//         age: 16
+//     },
+//     {
+//         name: "Bob Ziroll",
+//         age: 100
+//     }
+// ])); 
+
+// ================================================================================================================
+
+// The filter() method creates a new array filled with elements that pass a test provided by a function.
+// prb 1) Find the numbers in an array 
+
+// let arr1 = [45,25,15,11,23,27];
+// let newArr=  arr1.filter((value) => {
+//     return value<30;
+//   })
+//   console.log(newArr)
+
+// // Prb . 2)
+//  let ages = [18,25,14,22,45,33];
+//  let result = ages.filter(checkAdult);
+
+//  function checkAdult(age){
+//     return age >=18;
+//  }
+// console.log(result)
+
+// Prb 3) Given an array of numbers, return a new array that has only the numbers that are 5 or greater.
+
+// let numbrs = [7,10,8,2,1,5,40];
+// let newNumbrs = numbrs.filter(GreatrNumb);
+
+// function GreatrNumb(number){
+//     return number >=5
+// }
+// console.log(newNumbrs)
+
+// Prb. 4)  Given an array of numbers, return a new array that only includes the even numbers.
+
+// let num1 = [3,6,8,5,2,4];
+// let num2 = num1.filter(checkEven);
+
+// function checkEven(digit){
+//     return digit%2 ===0;
+// }
+// console.log(num2);
+
+// Prb 5.) Given an array of strings, return a new array that only includes those that are 5 characters or fewer in length
+
+// const fishes = ["Flipper","Sykes","Leny","Oscar","Salmon"];
+// const fishName = fishes.filter(checkLength);
+
+// function checkLength(str){
+//     return str.length >= 5;
+// }
+// console.log(fishName);
+
+// Prb .6) Given an array of people objects, return a new array that has filtered out all those 
+// who don't belong to the club.
+
+// const members = [
+//     { name: "Angelina Jolie", member: true },
+//     { name: "Eric Jones", member: false },
+//     { name: "Paris Hilton", member: true },
+//     { name: "Kayne West", member: false },
+//     { name: "Bob Ziroll", member: true }
+// ]
+// const member2 = members.filter(clubMember)
+
+// function clubMember(emp){
+//     return (emp.name, emp.member === true)
+// }
+// console.log(member2)
+
+// Prb. 7 Check the values of an array and return the values of in a new array if they are multiplies of 2.
+
+// const numb = ["20","10","11","23","33","50"]
+// const NewNumb = numb.filter(modulsuFunc);
+
+// function modulsuFunc(inp){
+//     return inp % 2 ==0;
+// }
+// console.log(NewNumb)
+
+// ===============================================================================================================
+
+//  The reduce() method executes a reducer function for array element.
+//The reduce() method returns a single value: the function's accumulated result.
+//The reduce() method does not execute the function for empty array elements.
+
+// Prb. 1) Turn an array of numbers into a total of all the numbers
+
+// let values = [11,22,33,44,55,00]
+// const add_Reduce = (num1,num2)=>{
+//     return num1 + num2;
+// }
+// let newValues = values.reduce(add_Reduce);
+// console.log(newValues);
+
+// Prb. 2) Turn an array of numbers into a long string of all those numbers.
+
+// let digits = [4,6,8,10,2];
+// const long_String = (h1,h2) => {
+//     return h1+h2;
+// }
+// let newDigits = digits.reduce(long_String);
+// console.log(newDigits);
+
+// Prb.  3) Subtract all numbers in an array:
+
+// let numbers = [44,10,5,2,0,6];
+// let newNumbers = numbers.reduce(subtract_num)
+// function subtract_num(num1,num2){
+// return num1 - num2;
+// }
+// console.log(newNumbers);
+
+// Prb .4) Round all the numbers and display the sum in a new array:
+
+// let decimal = [44.2,55.5,22.2,33.78,11.41];
+// let newDecimal = decimal.reduce(roundall_Numbers) // OR else we can use 0 as an arguments in reduce Function.
+// function roundall_Numbers(n1, n2){
+//     return Math.round(n1) + Math.round(n2);
+// }
+// console.log(newDecimal);
+
+// Prb. 5) Find the lowest number by using the reduce-function
+
+// let values = [22,55,101,78,4,66];
+// let newValues = values.reduce(lowest_Number);
+// function lowest_Number(h1 , h2){
+//     return Math.min(h1) < Math.min(h2);
+// }
+// console.log(newValues);
 
 
 
