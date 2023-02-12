9d72f14adc0e9307b319687069426310c5e3baaa
Pranay <pranaypatil6575@gmail.com>
2023-02-12 18:27:28 +0530

    Arrays Basic Programme added

--- Array/Arrays_Basic_prgrm.js
+++ Array/Arrays_Basic_prgrm.js
@@ -1,52 +1,67 @@
-let marks_12_cls = [87, 68, 74, 97, 88, null , true]
-console.log(marks_12_cls[0])
-console.log(marks_12_cls[1])
-console.log(marks_12_cls[2])
-console.log(marks_12_cls[3])
-console.log(marks_12_cls[4])
-console.log(marks_12_cls[5])
-console.log(marks_12_cls[6])
-console.log(marks_12_cls[7])
-console.log("The marks_12_cls  are "+ marks_12_cls.length)
-marks_12_cls[7] = 99 // Arrays are Mutable so it can changed //
-marks_12_cls[0] = 66 // Changed the valuse of an array index
-console.log(marks_12_cls)
-console.log(typeof marks_12_cls)
+// Prb, 1 Write a JavaScript function to check whether an 'input' is an array or not.
 
-let bank = ["icici", "kotak", "axis", "idbi" , "hdfc ", "sbi"]
-for(let i =0; i < bank.length; i++){
-  console.log(bank[i]);
-}
+// function is_Array(input){
+//     if(toString.call(input) == "[object Array]")
+//     return true;
+//     return false;
+// }
+// console.log(is_Array(['innovative'])) ; 
+// console.log(is_Array(['45781296'])) ; 
 
-// ===============================================================================================================================================
-// Array Methods
-let num = [ 45, 54, 58, 89 , 99]
-let noom = num.toString()              // noom is also now a String by toString()
-console.log(noom)
+// Prob. 2 Write a JavaScript function to clone an array.
+// let cloneArray = [4,6,8,10,12,14]
+// console.log(cloneArray);
+// console.log("Above is the Original Array");
+// let newClone = Array.from(cloneArray);
+// console.log("Below is the Clone Array")
+// console.log(newClone)
 
-let c = num.join(" & ")
-console.log(c, typeof c)
+// Prb 3 Write a JavaScript function to get the first element of an array. 
+//       Passing a parameter 'n' will return the first 'n' elements of the array.
 
-let d = num.pop()                      // Pop returns the pop element
-  console.log(num , d)
+// function removeItems(array , n){
+//     if (array == null)
+//     return 0;
+//     if(n == null)
+//     return array[0]
+//     if(n < 0)
+//     return []
+//     return array.slice(0,n)
+// };
 
-let e = num.push(55)                   // Push returns the new array length
-console.log(num, e )
+// console.log(removeItems([4,48,8,88,10],-1))
+// console.log(removeItems([45,52,11,80,101],2))
+// console.log(removeItems(["Cat","Dog","Horse","MOnkey","Snake"],3))
 
-let t = num.shift()                       // Shift remove the element
-console.log(t , num)
+// Prb 4. Write a simple JavaScript program to join all elements of the following array into a string.
+// function joinArray(inp){
+//     return joinArray.join();
+// }
+// console.log("Car","Truck","Plane","Jet","Train")
+                // OR
+//   let CompanyArray= ["Infosys","IBM","Capgemini","JP Morgan","M Stanley"]
+//   console.log(CompanyArray)
+//   console.log(CompanyArray.join())
+//   console.log(CompanyArray.join(" + "))
 
-let u = num.unshift(66)                        //  Unshift add the new length into the array
-console.log(u , num)
+// Prb 5.  Write a JavaScript program which accept a number as input and insert dashes (-) between each two even numbers. 
 
-let boys = ["John", "Martin", "Adam" , "Pop", "Jack"]
-let girls = ["Selena", "Sama", "Rebecca", "Ana"]
-let peer = boys.concat(girls)
-console.log(peer)
-boys.splice(1,3, "Robot", "Philip", "rock")
-console.log(boys);
+// const num = window.prompt("Enter Numb");
+// const str =num.toString();
+// const result = [str[0]];
 
+// for(let i=1; i<str.length; i++){
+//     if((str[i-1]%2 ==0) && (str[i]%2 == 0)){
+//         result.push('-')
+//     }
+//     else
+//     {
+//         result.push(str[i]);
+//     }
+// }
+// console.log(result.join(''))
 
-  
-
-
+// Prob. 6 How would you remove and return the last element from an array in JavaScript
+// let banks = ["Deutch","Citi","Yes","Doyce"]
+// banks.pop();
+// console.log(banks)
