a6f453520b7d4f31a9a1fd32624567ce8a8ea8b6
Pranay <pranaypatil6575@gmail.com>
2023-02-13 18:09:47 +0530

    "New array files added"

--- Array/Arrays_with_loop.js
+++ Array/Arrays_with_loop.js
@@ -1,22 +1,22 @@
 // NN SS BB U
-let a = null;
-let b = 500;
-let c = "Happy Holidays";
-let d = Symbol("I am Good");
-let e = true;
-let f = BigInt("315")+ BigInt("5")
-let g =  undefined;
+let type1 = null;
+let type2 = 500;
+let type3 = "Happy Holidays";
+let type4 = Symbol("I am Good");
+let type5 = true;
+let type6 = BigInt("315")+ BigInt("5")
+let type7 =  undefined;
 
-console.log(a,b,c,d,e,f,g);
-console.log(typeof c,a,g);
+console.log(type1,type2,type3,type4,type5,type6,type7);
+console.log(typeof type3,type4,type5);
 
 // Objects in JS
 
 // Non-Primitive Datatypes (Means Objects)
-const area = {
+const places = {
   "Thane": null,
   "Andheri" : undefined,
   "Dadar" : false,
   "Kurla" : 786 
 }
-console.log(area["Dadar"])
+console.log(places["Dadar"])
